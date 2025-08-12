f7accc72e51946af651754351ac9488d38f84b04
Sanjay Tandukar <sanjay.tandukar@imepay.com.np>
2025-08-12 07:52:23 +0545

    new screen added

---
+++ Sources/SplashPackage/Views/SplashScreen.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView 2.swift
+//  SplashPackage
+//
+//  Created by Sanjay Tandukar on 8/12/25.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        VStack(){
+            Text("Splash Screen")
+            Spacer()
+                .frame(height: 20)
+            Image(systemName: "globe")
+                .resizable()
+                .frame(width: 50, height: 50)
+        }
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
